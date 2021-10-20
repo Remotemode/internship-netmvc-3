69b319e6ce80da2707f08eed247c40f701dc1cc2
Anatoliy Ivanov <anatoliyi@123software.ru>
2021-10-20 15:43:56 +0300

    Project setup

---
+++ MobileWarehouse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MobileWarehouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
